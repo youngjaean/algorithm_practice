90f9d32d795b1cf48ca22936028121517b9422ef
youngjaean <yous4831@gmail.com>
2023-08-29 11:44:13 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,17 +1,3 @@
 def solution(operations):
     answer = []
-    for oper in operations:
-        if "I" in oper:
-            answer.append(int(oper[2:]))
-        elif "D 1" == oper and answer:
-            answer.sort()
-            answer.pop()
-        elif "D -1" == oper and answer:
-            answer.sort(reverse=True)
-            answer.pop()
-        else:
-            pass
-    if not answer:
-        return [0, 0]
-    else:
-        return max(answer), min(answer)
+    return answer
