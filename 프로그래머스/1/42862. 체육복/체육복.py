319a484888906aa2d96e30c7ace73f0e52f72f73
youngjaean <yous4831@gmail.com>
2023-10-14 15:57:15 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,25 +1,19 @@
 def solution(n, lost, reserve):
-    students = [1] * n
+    students = [0] + [1] * n
+    lost.sort()
     for l in lost:
-        students[l - 1] -= 1
+        students[l] -= 1
     for r in reserve:
-        students[r - 1] += 1
-    if students[0] == 0:
-        if students[1] > 1:
-            students[0] = 1
-            students[1] -= 1
-    for i in range(1, len(students) - 1):
-        if students[i] == 0:
-            if students[i - 1] > 1:
-                students[i] = 1
-                students[i - 1] -= 1
-            elif students[i + 1] > 1:
-                students[i] = 1
-                students[i + 1] -= 1
-    if students[-1] == 0:
-        if students[-2] > 1:
-            students[-1] = 1
-            students[-2] -= 1
+        students[r] += 1
+
+    for i in lost:
+        if i != 1 and students[i - 1] > 1:
+            students[i] = 1
+            students[i - 1] -= 1
+        elif i != n and students[i + 1] > 1:
+            students[i] = 1
+            students[i + 1] -= 1
+
     while 0 in students:
         students.remove(0)
     return len(students)
