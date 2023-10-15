23cfd396a5c4ae3c7f684ba45463c5875cb7f694
youngjaean <yous4831@gmail.com>
2023-10-15 18:24:38 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,20 +1,16 @@
+from collections import defaultdict
+
+
 def solution(genres, plays):
-    genrs_dict = {}
-
-    for genr, play in zip(genres, plays):
-        try:
-            genrs_dict[genr].append(play)
-        except:
-            genrs_dict[genr] = [play]
-    sum_list = []
-
-    for key in genrs_dict.keys():
-        sum_list.append([key, sum(genrs_dict[key])])
-    sum_list.sort(key=lambda x: x[1], reverse=True)
     answer = []
-    for key in sum_list:
-        for x in sorted(genrs_dict[key[0]], reverse=True)[:2]:
-            answer_index = plays.index(x)
-            answer.append(answer_index)
-            plays[answer_index] = -1
+    gen_dict = defaultdict(list)
+    play_sum = defaultdict(int)
+    for gn, pl in zip(genres, plays):
+        gen_dict[gn].append(pl)
+        play_sum[gn] += pl
+    for key in sorted(play_sum.keys(), key=lambda x: play_sum[x], reverse=True):
+        for value in sorted(gen_dict[key], key=lambda x: x, reverse=True)[0:2]:
+            index = plays.index(value)
+            answer.append(index)
+            plays[index] = -1
     return answer
