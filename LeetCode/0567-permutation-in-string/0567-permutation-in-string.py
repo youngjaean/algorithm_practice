b2463abdd9f13af08ed645627872dd6f2127106b
youngjaean <yous4831@gmail.com>
2024-10-11 11:35:18 +0900

    Time: 91 ms (35.56%), Space: 16.7 MB (70.68%) - LeetHub

--- LeetCode/0567-permutation-in-string/0567-permutation-in-string.py
+++ LeetCode/0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         len_s1, len_s2 = len(s1), len(s2)
-        
-        s1_count = Counter(s1)
-        window_count = Counter(s2[:len_s1])
-        if s1_count == window_count:
+
+        cnt = Counter(s1)
+        cnt_window = Counter(s2[:len_s1])
+
+        if cnt == cnt_window:
             return True
 
         for i in range(len_s1, len_s2):
-            window_count[s2[i]] += 1
+            cnt_window[s2[i]] += 1
+
             left_char = s2[i - len_s1]
-            window_count[left_char] -= 1
-            if window_count[left_char] == 0:
-                del window_count[left_char]
-  
-            if s1_count == window_count:
+            cnt_window[left_char] -= 1
+            if cnt_window[left_char] == 0:
+                del cnt_window[left_char]
+            if cnt == cnt_window:
                 return True
-        
+
         return False
