7684988445cbdc0c579f05d88b80bd7dd91c73c3
youngjaean <yous4831@gmail.com>
2025-09-12 09:30:30 +0900

    Time: 31 ms (5.04%), Space: 18.9 MB (95.71%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checker(char c){
+        return strchr("aeiou", c) != NULL;
+    }
+
+    bool doesAliceWin(string s) {
+        int turn = 1;
+        int vowels = 0;
+
+        for(auto c : s){
+            if(checker(c))
+                vowels += 1;
+        }
+
+        if (vowels != 0)
+            return true;
+        
+        else
+            return false;
+    }
+};
