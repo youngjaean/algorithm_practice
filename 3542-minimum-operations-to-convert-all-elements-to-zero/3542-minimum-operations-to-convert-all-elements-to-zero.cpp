eb4b57237a02fd38dfe17fdbe0d99c48bafc0bed
youngjaean <yous4831@gmail.com>
2025-11-10 22:27:46 +0900

    Time: 30 ms (77.53%), Space: 221.7 MB (92.98%) - LeetHub

---
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums)
+    {
+        vector<int> s;
+        int res = 0;
+        for (int a : nums)
+        {
+            while (!s.empty() && s.back() > a)
+            {
+                s.pop_back();
+            }
+            if (a == 0)
+                continue;
+            if (s.empty() || s.back() < a)
+            {
+                ++res;
+                s.push_back(a);
+            }
+        }
+        return res;
+    }
+};
