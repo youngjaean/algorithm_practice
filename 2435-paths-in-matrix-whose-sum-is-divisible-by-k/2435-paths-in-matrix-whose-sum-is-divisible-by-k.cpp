83ae2a3ff0e840c3f612ac298740cad7a741dc6f
youngjaean <yous4831@gmail.com>
2025-11-26 17:14:08 +0900

    Time: 273 ms (48.58%), Space: 224.1 MB (5.21%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        const int MOD = 1e9 + 7;
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        auto dp = vector(m + 1, vector(n + 1, vector<long long>(k)));
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 && j == 1) {
+                    dp[i][j][grid[0][0] % k] = 1;
+                    continue;
+                }
+
+                int value = grid[i - 1][j - 1] % k;
+                for (int r = 0; r < k; r++) {
+                    int prevMod = (r - value + k) % k;
+                    dp[i][j][r] =
+                        (dp[i - 1][j][prevMod] + dp[i][j - 1][prevMod]) % MOD;
+                }
+            }
+        }
+
+        return dp[m][n][0];
+    }
+};
