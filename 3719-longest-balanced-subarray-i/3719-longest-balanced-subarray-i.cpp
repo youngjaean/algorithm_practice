bda0e6275a13d9da8c207b7b8f4dfad96782266e
youngjaean <yous4831@gmail.com>
2026-02-10 09:37:27 +0900

    Time: 1835 ms (79.04%), Space: 530.6 MB (12.58%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,41 +1,22 @@
-#include <vector>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
-        // unique set
-        unordered_set<int> unique_nums(nums.begin(), nums.end());
+        size_t len = 0;
 
-        int odd = 0;
-        int even = 0;
+        for (size_t i = 0; i < nums.size(); i++) {
+            auto odd = unordered_map<int, int>();
+            auto even = unordered_map<int, int>();
 
-        for (int num : unique_nums)
-        {
-            if (num % 2 == 0)
-                even++;
-            else
-                odd++;
+            for (size_t j = i; j < nums.size(); j++) {
+                auto& c = (nums[j] & 1) ? odd : even;
+                c[nums[j]]++;
+
+                if (odd.size() == even.size()) {
+                    len = std::max(len, j - i + 1);
+                }
+            }
         }
 
-        unordered_map<int, int> freq;
-        for (int num : nums)
-        {
-            freq[num]++;
-        }
-
-        int max_duplicate = 0;
-        for (const auto& [value, count] : freq)
-        {
-            if (count > 1)
-                max_duplicate = max(max_duplicate, count - 1);
-        }
-        if (even != 0 && odd != 0 )
-            return min(even, odd) * 2 + max_duplicate;
-        else return 0;
+        return int(len);
     }
 };
