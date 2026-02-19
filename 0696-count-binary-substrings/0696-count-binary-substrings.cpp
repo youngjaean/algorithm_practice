7db562d007f10b2996b90ef7f6d48fb48fbe9115
youngjaean <yous4831@gmail.com>
2026-02-19 09:21:47 +0900

    Time: 0 ms (100%), Space: 13.2 MB (32.17%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int prev = 0, curr = 1, res = 0, n = s.length();
+        for (int i = 1; i < n; ++i) {
+            if (s[i] == s[i - 1]) ++curr;
+            else {
+                res += min(prev, curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+        res += min(prev, curr);
+        return res;
+    }
+};
