e01e6ce3514f777bf5b3315173c84695a0a116d8
youngjaean <yous4831@gmail.com>
2026-02-22 10:30:53 +0900

    Time: 0 ms (100%), Space: 8 MB (70.75%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int last = -1;
+        int ans = 0;
+
+        for (int i = 0; i < 32; ++i) {
+            if ((n >> i) & 1) {
+                if (last != -1)
+                    ans = max(ans, i - last);
+                last = i;
+            }
+        }
+
+        return ans;
+    }
+};
