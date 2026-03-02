c1575e80e467b8e42f50871ad6a3b97664c40377
youngjaean <yous4831@gmail.com>
2026-03-02 18:50:33 +0900

    Time: 4 ms (21.95%), Space: 30.1 MB (24.39%) - LeetHub

---
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minSwaps(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> maxRight(n, -1);
+
+        // 각 row의 가장 오른쪽 1의 index 찾기
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 1) {
+                    maxRight[i] = j;
+                    break;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int k = -1;
+
+            for (int j = i; j < n; j++) {
+                if (maxRight[j] <= i) {
+                    ans += j - i;
+                    k = j;
+                    break;
+                }
+            }
+
+            if (k == -1) return -1;
+
+            for (int j = k; j > i; --j) {
+                swap(maxRight[j], maxRight[j - 1]);
+            }
+        }
+
+        return ans;
+    }
+};
