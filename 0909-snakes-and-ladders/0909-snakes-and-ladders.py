fbed2719b06ad49eaa19003160f938a510f0e999
youngjaean <yous4831@gmail.com>
2026-04-22 21:49:35 +0900

    Time: 23 ms (53.35%), Space: 19.5 MB (8.06%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,32 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        vist = set([1])
+
+        def gen_grid(pos):
+            r, c = divmod(pos - 1, n)
+            row = n - 1 - r
+            col = c if r % 2 == 0 else n - c - 1
+            return row, col
+
+        queu = deque([(1, 0)])
+
+        while queu:
+            pos, move = queu.popleft()
+            for i in range(1, 7):
+                next_pos = pos + i
+                if next_pos > n * n:
+                    continue
+
+                row, col = gen_grid(next_pos)
+                if board[row][col] != -1:
+                    next_pos = board[row][col]
+
+                if next_pos == n * n:
+                    return move + 1
+
+                if next_pos not in vist:
+                    vist.add(next_pos)
+                    queu.append((next_pos, move + 1))
+
+        return -1
