8c277e41b7648a75fa5388b6e335e5dcc2aa8d60
youngjaean <yous4831@gmail.com>
2026-04-24 09:36:23 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,43 +3,41 @@ from typing import List
 
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
         if not board or not board[0]:
             return
 
         n, m = len(board), len(board[0])
-        visited = set()
 
-        for X in range(n):
-            for Y in range(m):
-                if board[X][Y] == "O" and (X, Y) not in visited:
-                    check = True
-                    change = []
-                    stack = [(X, Y)]
-                    visited.add((X, Y))
+        def dfs(x: int, y: int) -> None:
+            stack = [(x, y)]
+            board[x][y] = "S"
 
-                    while stack:
-                        x, y = stack.pop()
-                        change.append((x, y))
+            while stack:
+                x, y = stack.pop()
 
-                        if x == 0 or x == n - 1 or y == 0 or y == m - 1:
-                            check = False
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    nx = x + dx
+                    ny = y + dy
 
-                        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                            nx = x + dx
-                            ny = y + dy
+                    if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == "O":
+                        board[nx][ny] = "S"
+                        stack.append((nx, ny))
 
-                            if (
-                                0 <= nx < n
-                                and 0 <= ny < m
-                                and board[nx][ny] == "O"
-                                and (nx, ny) not in visited
-                            ):
-                                visited.add((nx, ny))
-                                stack.append((nx, ny))
+        for i in range(n):
+            if board[i][0] == "O":
+                dfs(i, 0)
+            if board[i][m - 1] == "O":
+                dfs(i, m - 1)
 
-                    if check:
-                        for x, y in change:
-                            board[x][y] = "X"
+        for j in range(m):
+            if board[0][j] == "O":
+                dfs(0, j)
+            if board[n - 1][j] == "O":
+                dfs(n - 1, j)
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "S":
+                    board[i][j] = "O"
