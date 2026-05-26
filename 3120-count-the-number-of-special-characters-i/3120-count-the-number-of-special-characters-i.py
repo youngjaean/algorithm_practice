1512ed2d74fc9dc6f48ce0f190e6c02d6dc0e7ba
youngjaean <yous4831@gmail.com>
2026-05-26 16:46:01 +0900

    Time: 3 ms (37.34%), Space: 19.4 MB (25.31%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,17 +1,4 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        count = 0
-        visited = []
-
-        for i in range(len(word) - 1):
-            for j in range(i + 1, len(word)):
-                if (
-                    abs(ord(word[i]) - ord(word[j])) == 32
-                    and word[i] not in visited
-                    and word[j] not in visited
-                ):
-                    count += 1
-                    visited.extend([word[i], word[j]])
-                    break
-
-        return count
+        s = set(word)
+        return sum(c in s and c.upper() in s for c in string.ascii_lowercase)
